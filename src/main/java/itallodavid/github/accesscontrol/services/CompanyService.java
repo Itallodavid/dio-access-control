a90657aae8585969ce7b30972cb0123e9a0fb2cb
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-08-20 16:07:52 -0300

    Add getCompany method and update createCompany

--- src/main/java/itallodavid/github/accesscontrol/services/CompanyService.java
+++ src/main/java/itallodavid/github/accesscontrol/services/CompanyService.java
@@ -1,6 +1,8 @@
 package itallodavid.github.accesscontrol.services;
 
 import itallodavid.github.accesscontrol.dto.CompanyCreationDTO;
+import itallodavid.github.accesscontrol.exceptions.EntityAlreadyExistsException;
+import itallodavid.github.accesscontrol.exceptions.EntityNotFoundException;
 import itallodavid.github.accesscontrol.mappers.CompanyMapper;
 import itallodavid.github.accesscontrol.models.Company;
 import itallodavid.github.accesscontrol.repositories.CompanyRepository;
@@ -9,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -22,8 +26,25 @@ public class CompanyService {
         return repository.findAll(pageable);
     }
 
+    @Transactional(readOnly = true)
+    public Company getCompany(final String cnpj) throws EntityNotFoundException {
+        Optional<Company> company = repository.findById(cnpj);
+
+        if(company.isEmpty()){
+            throw new EntityNotFoundException();
+        }
+
+        return company.get();
+    }
+
     @Transactional
-    public Company createCompany(final CompanyCreationDTO companyCreationDTO) {
+    public Company createCompany(final CompanyCreationDTO companyCreationDTO) throws EntityAlreadyExistsException {
+        Optional<Company> existingCompany = repository.findById(companyCreationDTO.getCnpj());
+
+        if(existingCompany.isPresent()){
+            throw new EntityAlreadyExistsException();
+        }
+
         Company company = mapper.toEntity(companyCreationDTO);
         return repository.save(company);
     }
