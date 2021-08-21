d33070727c0fed830e2a583ae15906d717b80a51
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-08-20 23:10:50 -0300

    ResponseException extends Throwable

--- src/main/java/itallodavid/github/accesscontrol/exceptions/ResponseException.java
+++ src/main/java/itallodavid/github/accesscontrol/exceptions/ResponseException.java
@@ -2,7 +2,7 @@ package itallodavid.github.accesscontrol.exceptions;
 
 import org.springframework.http.HttpStatus;
 
-public abstract class ResponseException extends RuntimeException {
+public abstract class ResponseException extends Throwable {
     abstract public String getError();
     abstract public String getDescription();
     abstract public HttpStatus getHttpStatus();
