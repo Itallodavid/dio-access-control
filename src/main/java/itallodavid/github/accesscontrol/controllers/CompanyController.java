d0bc418a61361906c404527a86b47b2fdc48291c
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-08-20 23:00:10 -0300

    Add endpoint PUT /{id}

--- src/main/java/itallodavid/github/accesscontrol/controllers/CompanyController.java
+++ src/main/java/itallodavid/github/accesscontrol/controllers/CompanyController.java
@@ -1,6 +1,9 @@
 package itallodavid.github.accesscontrol.controllers;
 
 import itallodavid.github.accesscontrol.dto.CompanyCreationDTO;
+import itallodavid.github.accesscontrol.dto.CompanyUpdateDTO;
+import itallodavid.github.accesscontrol.exceptions.EntityAlreadyExistsException;
+import itallodavid.github.accesscontrol.exceptions.EntityNotFoundException;
 import itallodavid.github.accesscontrol.models.Company;
 import itallodavid.github.accesscontrol.services.CompanyService;
 import lombok.AllArgsConstructor;
@@ -24,13 +27,21 @@ public class CompanyController {
     }
 
     @GetMapping(path = "/{cnpj}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Company getCompany(final @PathVariable String cnpj) {
+    public Company getCompany(final @PathVariable String cnpj) throws EntityNotFoundException {
         return service.getCompany(cnpj);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public Company createCompany(@Valid @RequestBody final CompanyCreationDTO dto){
+    public Company createCompany(@Valid @RequestBody final CompanyCreationDTO dto) throws EntityAlreadyExistsException {
         return service.createCompany(dto);
+    }
+
+    @PutMapping(path = "/{cnpj}",
+        consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Company updateCompany(
+        @PathVariable final String cnpj,
+        @Valid @RequestBody final CompanyUpdateDTO dto) throws EntityNotFoundException {
+        return service.updateCompany(cnpj, dto);
     }
 }
