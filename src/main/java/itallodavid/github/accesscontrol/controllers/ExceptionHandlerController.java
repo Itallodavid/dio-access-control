83c86e4b0cf080168e46c9849bb3d9964cdae021
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-08-20 22:58:38 -0300

    Add temporary implementation of global exception

--- src/main/java/itallodavid/github/accesscontrol/controllers/ExceptionHandlerController.java
+++ src/main/java/itallodavid/github/accesscontrol/controllers/ExceptionHandlerController.java
@@ -4,12 +4,16 @@ import itallodavid.github.accesscontrol.dto.ResponseExceptionDTO;
 import itallodavid.github.accesscontrol.exceptions.ResponseException;
 import itallodavid.github.accesscontrol.mappers.ResponseExceptionMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 @AllArgsConstructor
 public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
 
@@ -19,4 +23,12 @@ public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
     protected ResponseEntity<ResponseExceptionDTO> handlerResponseException(final ResponseException exception) {
         return ResponseEntity.status(exception.getHttpStatus()).body(mapper.toDTO(exception));
     }
+
+    // temporary implementation
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+        Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), headers, status);
+    }
+
 }
