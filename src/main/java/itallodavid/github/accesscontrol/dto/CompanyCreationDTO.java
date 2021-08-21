24861497fc328b6212eef48a86b54749b87c6f8e
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-08-20 23:08:58 -0300

    Small change in CompanyCreationDTO

--- src/main/java/itallodavid/github/accesscontrol/dto/CompanyCreationDTO.java
+++ src/main/java/itallodavid/github/accesscontrol/dto/CompanyCreationDTO.java
@@ -5,18 +5,20 @@ import lombok.Setter;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Getter @Setter
 public class CompanyCreationDTO implements Serializable {
+
     @NotEmpty @Size(min = 14, max = 14) @Pattern(regexp = "\\d{14}")
     private String cnpj;
 
-    @Valid
-    private AddressDTO address;
+    @Valid @NotNull
+    protected AddressDTO address;
 
     @Size(min = 12, max = 13)
-    private String telephone;
+    protected String telephone;
 }
